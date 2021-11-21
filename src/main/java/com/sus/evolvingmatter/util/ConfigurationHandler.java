b16cec03b839c7652a8239f82a5b5a39062e9698
Lior <lior.buchmeier@gmail.com>
2021-11-21 14:45:53 +0200

    backup before adding geckoLib

--- src/main/java/com/sus/evolvingmatter/util/ConfigurationHandler.java
+++ src/main/java/com/sus/evolvingmatter/util/ConfigurationHandler.java
@@ -14,28 +14,7 @@ import static net.minecraftforge.common.BiomeDictionary.Type.*;
 
 public class ConfigurationHandler {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-    public static final General GENERAL = new General(BUILDER);
     public static final Spawn SPAWN = new Spawn(BUILDER);
-
-    public static class General {
-        public final ForgeConfigSpec.BooleanValue dropFish;
-        public final ForgeConfigSpec.BooleanValue dropExp;
-        public final ForgeConfigSpec.ConfigValue<List<? extends String>> spawnBlocks;
-
-        General(ForgeConfigSpec.Builder builder) {
-            builder.push("general");
-            dropFish = builder
-                    .comment("Enable that penguins drop fish (0 - 2 Raw Cod)")
-                    .translation("waddles.configgui.dropFish")
-                    .define("dropFish", false);
-            dropExp = builder
-                    .comment("Penguins should drop experience?")
-                    .translation("waddles.configgui.dropExp")
-                    .define("dropExp", true);
-            spawnBlocks = builder.defineList("spawn blocks", Collections.singletonList(Blocks.GRASS_BLOCK.getRegistryName().toString()), o -> o instanceof String && ForgeRegistries.BLOCKS.getKeys().contains(new ResourceLocation(o.toString())));
-            builder.pop();
-        }
-    }
 
     public static class Spawn {
         public final ForgeConfigSpec.IntValue min;
